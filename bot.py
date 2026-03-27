380447d5324e33f414075fbba9c56f69a8ceb4cd
losserak379-cyber <losserak379@gmail.com>
2026-03-27 10:28:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import yt_dlp
 
-BOT_TOKEN = "PASTE_YOUR_TOKEN_HERE"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
@@ -15,10 +15,8 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("⏳ Downloading...")
 
     ydl_opts = {
-    'format': 'best[filesize<50M]',
-    'outtmpl': 'video.%(ext)s',
-    'noplaylist': True,
-    'quiet': True
+        'format': 'best',
+        'outtmpl': 'video.%(ext)s'
     }
 
     try:
@@ -29,11 +27,10 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_video(video=open(file, 'rb'))
         os.remove(file)
 
-    except Exception as e:
+    except:
         await update.message.reply_text("❌ Failed to download")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT, download))
 
-print("Bot running...")
 app.run_polling()
